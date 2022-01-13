9b673c02a11ca9b8865dc752672b0a4e1ba9741f
nbowd <bowdenn@oregonstate.edu>
2022-01-12 21:09:39 -0800

    modifications for aws deployment

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 Alpha Energy Microserivce
 
-This is web scraping service I built for a teammate in a Software Engineering class at OSU. 
-Try it here: https://osu361rad.herokuapp.com/?z=96&a=246
-z - Atomic number, required
+This is web scraping service I built for a teammate in a Software Engineering class at OSU.
+
+<b>Try it here:</b> https://osu361rad.herokuapp.com/?z=96&a=246 \
+z - Atomic number, required\
 a - Mass number, optional
 
-Note: This is hosted on Heroku, it may take a few extra seconds to 'spin up' if it has been inactive for too long
+<i>Note: This is hosted on Heroku, it may take a few extra seconds to 'spin up' if it has been inactive for too long</i>
 
-The scraper is built using Node and Puppeteer, a JavaScript library that allows for browser automation and scraping. It also uses this website to find nuclide information: http://nucleardata.nuclear.lu.se/toi/nucSearch.asp 
+The scraper is built using Node and Puppeteer, a JavaScript library that allows for browser automation and scraping. It also uses this website to find nuclide information: http://nucleardata.nuclear.lu.se/toi/nucSearch.asp
 
 When the service receives a GET request, it parses 'z' and 'a' from above, using that information to find the correct radionuclide. It will navigate through all the sub-links to scrape Alpha Energy/Intensity information. Once finished, it returns the entire list of data found.
 
-This scraper can be imported into JavaScript as a function using 'scraper.js' e.g: findDecay(96, 246) 
+This scraper can be imported into JavaScript as a function using 'scraper.js' e.g: findDecay(96, 246)
--- scraper.js
+++ scraper.js
@@ -1,75 +1,89 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
 const searchLinks = async (url, page) => {
-  await page.goto(url)
-  
+  await page.goto(url);
+
   // Two list representing two columns for each row.
-  const allDecayModes = await page.$$eval('tr td:nth-last-of-type(5)', t=>t.map(e => e.textContent))
-  const allLinks = await page.$$eval('tr th a', allAs => allAs.map(a => a.href))
+  const allDecayModes = await page.$$eval('tr td:nth-last-of-type(5)', (t) =>
+    t.map((e) => e.textContent)
+  );
+  const allLinks = await page.$$eval('tr th a', (allAs) =>
+    allAs.map((a) => a.href)
+  );
 
   // Loops through each Decay Mode and if it contains the alpha symbol 'a', then the corresponding href link is saved.
-  const links = []
+  const links = [];
   for (const [index, element] of allDecayModes.entries()) {
-    if (element.includes('a')){
-      links.push(allLinks[index])
+    if (element.includes('a')) {
+      links.push(allLinks[index]);
     }
   }
 
-  return links
-}
+  return links;
+};
 
-const parseDecay = async(url, page) => {
-  await page.goto(url)
+const parseDecay = async (url, page) => {
+  await page.goto(url);
   // Try/Catch handles situations where there are no decay tables present because $eval selectors throw an error when no match is found.
-  try{
-    let alpha = await page.$$eval('td center table:nth-last-of-type(1) tr td', t => t.map(e => e.textContent.split(String.fromCharCode(160))))
+  try {
+    let alpha = await page.$$eval(
+      'td center table:nth-last-of-type(1) tr td',
+      (t) => t.map((e) => e.textContent.split(String.fromCharCode(160)))
+    );
 
     // Removes first and last element which are blank from scraping page breaks
-    alpha = await alpha.slice(1, alpha.length - 1)
+    alpha = await alpha.slice(1, alpha.length - 1);
 
-    const ea = []
-    const ia = []
+    const ea = [];
+    const ia = [];
 
     // Pairs Energies and Intensities in sub-lists
-    for (let i=0; i< alpha.length; i=i+2){
-      ea.push(alpha[i][0])
-      ia.push(alpha[i+1][0])
+    for (let i = 0; i < alpha.length; i = i + 2) {
+      ea.push(alpha[i][0]);
+      ia.push(alpha[i + 1][0]);
     }
 
-    return {ea: ea.reverse(), ia: ia.reverse()}
-
-  } catch (error){
-    return null
+    return { ea: ea.reverse(), ia: ia.reverse() };
+  } catch (error) {
+    return null;
   }
-}
+};
 
-const findDecay = async (z=null, a=null) => {
-  const baseUrl = 'http://nucleardata.nuclear.lu.se/toi/listnuc.asp?sql=&'
-  const payload = []
+const findDecay = async (z = null, a = null) => {
+  const baseUrl = 'http://nucleardata.nuclear.lu.se/toi/listnuc.asp?sql=&';
+  const payload = [];
 
-  // Adjusts URL based on passed parameters. 
-  let url = null 
-  if(!z) {return null}  // No parameters passed
-  else if (!a){url = `${baseUrl}Z=${z}`}  // Only atomic number passed
-  else {url = `${baseUrl}A1=${a}&A2=${a}&Z=${z}`}  // Mass number and atomic number passed
-  
+  // Adjusts URL based on passed parameters.
+  let url = null;
+  if (!z) {
+    return null;
+  } // No parameters passed
+  else if (!a) {
+    url = `${baseUrl}Z=${z}`;
+  } // Only atomic number passed
+  else {
+    url = `${baseUrl}A1=${a}&A2=${a}&Z=${z}`;
+  } // Mass number and atomic number passed
+
   // For debugging: pass {headless:false} to launch() to display the automation
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  })  
-  const page = await browser.newPage()
-  
-  const links = await searchLinks(url, page)
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+
+  const links = await searchLinks(url, page);
 
   // Parses alpha energy information from each page collected
-  for (let link of links){
-    let info = await parseDecay(link, page)
-    if (info){ payload.push({ea: info.ea, ia: info.ia}) }
+  for (let link of links) {
+    let info = await parseDecay(link, page);
+    if (info) {
+      payload.push({ ea: info.ea, ia: info.ia });
+    }
   }
-  
-  await browser.close()
 
-  return payload
-}
+  await browser.close();
 
-module.exports = findDecay
+  return payload;
+};
+
+module.exports = findDecay;
